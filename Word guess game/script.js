f5db549342d7b3afd0025f0e3c1edbc5f92de7e5
BaeedKhan91 <baeedk7@gmail.com>
2025-10-02 15:28:12 +0500

    Word Guessing Game

---
+++ Word guess game/script.js
@@ -0,0 +1,245 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
+let resetBtn = document.getElementById("btn")
+let input = document.getElementById("input");
+let inputsElem = document.querySelector(".inputs");
+let hintEle = document.querySelector(".hint span");
+let keyInput = document.querySelector("#keyinput");
+
+let word;
+
+function randomWord(){
+    let randObj = wordList[Math.floor(Math.random()* wordList.length)]
+    word = randObj.word
+    let hint = randObj.hint
+    console.log(word);
+
+    let html =""
+    for(let i =0; i < word.length; i++ ){
+        html += `  <input type="text"  disabled>`
+    }
+    hintEle.innerHTML = hint
+    
+    inputsElem.innerHTML = html
+
+}
+
+function initGame(e){
+    let key = e.target.value;
+    if (key.match(/^[a-zA-Z]$/)) {
+        
+        console.log(key);
+        if (word.includes(key)) {
+            console.log("correct");
+            
+        }else{
+            console.log("incirrect");
+            
+        }
+    }
+    keyInput.value =""
+    
+}
+
+
+resetBtn.addEventListener("click",randomWord)
+keyInput.addEventListener("input",initGame  )
+document.addEventListener("keydown",() => keyInput.focus())
+
+
